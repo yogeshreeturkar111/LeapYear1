4f5384d95a099aa179494f85cc21ad3bcc0a94ac
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-02 22:24:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LeapYear
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (Year % 4 == 0)
+                Console.WriteLine("{0} is a Leap Year.", Year);
+            else Console.WriteLine("{0} is not a Leap Year.", Year);
+            Console.ReadLine();
+        }
+    }
+}
